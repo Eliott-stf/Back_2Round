053261440178d4a985b7824e1aee7b05128f7a4e
ES_PREPA_DEV <setifeliott@gmail.com>
2026-05-11 13:39:27 +0200

    add: PrismaClient connection on mariadb

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super({
+            adapter: new PrismaMariaDb({
+                url: process.env.DATABASE_URL!,
+            } as any),
+            log: ['info', 'warn', 'error'],
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
